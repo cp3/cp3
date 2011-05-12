94ff42a59281e00c542cc727e838d090e9abaf98
Mark <devine38@gmail.com>
2011-05-12 10:29:19 -0700

    Cleaned up troggy a bit

--- trogdor.c
+++ trogdor.c
@@ -86,6 +86,8 @@ void printBoard(int* testBoard)
 int isWin(int* testBoard, int lastColumn)
 {
 	int i, j, k, lastRow = rows-1, top, bot, left, right;
+	// Total and maximal win points for red and blue
+	int totalRed = 0, totalBlue = 0, maxRed = 0, maxBlue = 0;
 	// Find height of last piece played
 	for (i=0; i < rows; i++) {
 		//printf("%d ",testBoard[i+lastColumn*rows]);
@@ -159,34 +161,7 @@ int isWin(int* testBoard, int lastColumn)
 	}
 	printf("\n");
 
-	/*int horizontal = rows*(columns-3), vertical = columns*(rows-3), diagonal = 2*(rows-3)*(columns-3);
-	char quads[horizontal+vertical+diagonal][4];
-	printf("Horizontal: %d\nVertical %d\nDiagonal %d\n",horizontal, vertical, diagonal);
-	// Do horizontal sections
-	for (i = rows-1; i > 0; i--) {
-		for (j = 0; j < columns-3; j++) {
-			for (k = 0; k < 4; k++, l++) {
-				//quads[l][k] = testBoard[i+(j+k)*rows];
-				//printf("%c ", testBoard[i+(j+k)*rows]);
-			}
-			//printf("\n");
-		}
-		//printf("\n");
-	}
-	// TODO: Do vertical sections
-	for (i = 0; i < columns; i++) {
-		for (j = 0; j < rows-3; j++) {
-			for (k = 0; k < 4; k++, l++) {
-				//quads[l][k] = testBoard[k+j+i*rows];
-				printf("%c ", testBoard[k+j+i*rows]);
-				break;
-			}
-			printf("\n");
-		}
-		printf("\n");
-	}*/
 	return 0;
-	// TODO: Do diagonal sections
 } /* isWin */
 
 /******************************************************************************\
